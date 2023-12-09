24e9dfa6553d5868cf861be597301d120f4f0256
fritzfhwn <fritz.thoendl@gmx.at>
2023-12-09 18:37:55 +0100

    changing code to extract raw, unfiltered data, adding example json files of search / video / channel output, combining them to one dataset per search result (complete_list)

--- dataextraction.py
+++ dataextraction.py
@@ -9,6 +9,7 @@ end_time = datetime(year=2008, month=1, day=1).strftime('%Y-%m-%dT%H:%M:%SZ')
 api_key = "AIzaSyBTJ-3GnxHQStbUBxI8cPdxMjGRDnV6iAY"
 search_api = "https://www.googleapis.com/youtube/v3/search"
 video_api = "https://www.googleapis.com/youtube/v3/videos"
+channel_api = "https://www.googleapis.com/youtube/v3/channels"
 categories_api = "https://www.googleapis.com/youtube/v3/videoCategories"
 
 
@@ -26,61 +27,62 @@ def get_category_list():
 
 categories = json.load(open("category_list.json"))
 
+
+def get_channel_details_by_video_list(video_list):
+    for item in video_list["items"]:
+        channel_args = {
+            "part": "snippet,contentDetails,statistics,brandingSettings,contentOwnerDetails,"
+                    "localizations,status,topicDetails",
+            "id": item["snippet"]["channelId"],
+            "key": api_key
+        }
+        channel_response = requests.get(channel_api, channel_args).json()
+        item["channel_details"] = channel_response["items"]
 def get_video_statistics_by_video_list(video_list):
     #function receives the prepared video list from search filter
     #will add further video details and statistics to the list which are not to be found in
     # the search queue
-    for item in video_list["video_results"]:
-        for entry in item:
-            video_args = {
-                "part": "snippet,contentDetails,statistics",
-                "id": entry["videoId"],
-                "key": api_key
+    for item in video_list["items"]:
+        # fileDetails, processingDetails, suggestions - only for content owner
+        video_args = {
+            "part": "snippet,contentDetails,statistics,liveStreamingDetails,"
+                    "localizations,player,recordingDetails,status,"
+                    "topicDetails",
+            "id": item["id"]["videoId"],
+            "key": api_key
             }
-            video_response = requests.get(video_api, video_args).json()
-            for category in categories:
-                if category["id"] == video_response["items"][0]["snippet"]["categoryId"]:
-                    entry["category"] = category["name"]
-            entry["tags"] = video_response["items"][0]["snippet"]["tags"]
-            entry["contentDetails"] = video_response["items"][0]["contentDetails"]
-            entry["statistics"] = video_response["items"][0]["statistics"]
+        video_response = requests.get(video_api, video_args).json()
+        for category in categories:
+            if category["id"] == video_response["items"][0]["snippet"]["categoryId"]:
+                video_response["items"][0]["snippet"]["categoryId"] = category["name"]
+        item["video_details"] = video_response
 
-def get_video_list_by_search_filter(search_string, region, start_date, end_date):
+def get_video_list_by_search_filter(search_string):
     search_args = {
         "part": "snippet",
         "q": search_string,
         "type": "video",
-        "regionCode": region,
-        "publishedAfter": start_date,
-        "publishedBefore": end_date,
-        "maxResults": 5,
+        "maxResults": 10,
         "key": api_key,
         "pageToken": None
     }
 
     counter = 1
     filtered_search_list = {
-        "region": region,
-        "video_results": []
-    }
-    while counter < 3:
+        "header": [],
+        "items": []}
+    while counter < 5:
         search_list = requests.get(search_api, search_args).json()
+        filtered_search_list["header"] = [{item: search_list[item]} for item in search_list.keys() if item != "items"]
         if search_args["pageToken"] == search_list["nextPageToken"]:
             break
         search_args["pageToken"] = search_list["nextPageToken"]
-        filtered_search_list["video_results"].append([item for item in [
-            {"videoId": entry["id"]["videoId"], "videoTitle": entry["snippet"]["title"],
-             "description": entry["snippet"]["description"],
-             "channelTitle": entry["snippet"]["channelTitle"],
-             "channelId": entry["snippet"]["channelId"],
-             "publishedAt": entry["snippet"]["publishedAt"],
-             "liveBroadcast": entry["snippet"][
-                 "liveBroadcastContent"]} for entry in search_list["items"]]])
+        for item in search_list["items"]:
+            filtered_search_list["items"].append(item)
         counter += 1
-
     get_video_statistics_by_video_list(filtered_search_list)
-    return json.dump(filtered_search_list, open(f"video_list_{search_string}", "w"), indent=6)
+    get_channel_details_by_video_list(filtered_search_list)
+    json.dump(filtered_search_list, open(f"complete_list_{search_string}", "w"), indent=6)
 
 
-
-get_video_list_by_search_filter("food blog", "US", start_time, end_time)
+get_video_list_by_search_filter("fitness")
